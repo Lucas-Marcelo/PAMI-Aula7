e40223cde346f17fadca7b39f66294cd93a6e4b0
Lucas-Marcelo <sonsofmaria@outlook.com>
2019-09-30 22:28:16 -0300

    Ações nos botões da ActionBar.

--- app/src/main/java/br/com/etechoracio_actionbar/ActionBarActivity.java
+++ app/src/main/java/br/com/etechoracio_actionbar/ActionBarActivity.java
@@ -3,7 +3,11 @@ package br.com.etechoracio_actionbar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class ActionBarActivity extends AppCompatActivity {
 
@@ -11,5 +15,41 @@ public class ActionBarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_bar);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.actions,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.action_wifi:
+                Toast.makeText(this, "Wi-Fi acionado!", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_bluetooth:
+                Toast.makeText(this, "Bluetooth acionado!", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_sobre:
+                Toast.makeText(this, "Sobre acionado!", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_sair:
+                Toast.makeText(this, "Sair acionado!", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
